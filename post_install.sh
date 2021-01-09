d9bbcf08b21bfdd29d8b88f9a06ca6a95d121ab1
C4ne <32538912+C4ne@users.noreply.github.com>
2021-01-09 22:34:25 +0100

    Added SSL

--- post_install.sh
+++ post_install.sh
@@ -13,6 +13,26 @@ service postgresql start
 USER="wikijs"
 DB="wikijs_production"
 DOCUMENTROOT="/usr/local/www/wikijs"
+INTERFACE="$(route get default | awk '$1 == "interface:" {print $2}')"
+IPV4="$(ifconfig $if | awk '$1 == "inet" {print $2}')"
+SSL_KEY="/etc/ssl/private/key.pem"
+SSL_CERT="/etc/ssl/certs/cert.pem"
+
+if ! route get "$IPV4"; then
+    echo "IP address \'$IPV4\' doesn't seem to be valid or accssible on this host"
+    exit 1
+fi
+
+# Generate SSL certificate and key
+mkdir /etc/ssl/private
+chmod -R 755 /etc/ssl/private
+openssl req -nodes -x509 -newkey rsa:4096 -keyout $SSL_KEY \
+  -out $SSL_CERT -days 365\
+  -subj "/C=DE/ST=Berlin/L=Berlin/O=BND/OU=Abteilung 8/"
+chmod 400 "$SSL_KEY"
+chmod 400 "$SSL_CERT"
+chown wikijs "$SSL_KEY"
+chown wikijs "$SSL_CERT"
 
 # Add a user who will run node
 pw useradd -n "$USER" -d /nonexistent -s /usr/sbin/nologin -c "User that runs Wiki.js"
@@ -43,9 +63,19 @@ tar xzf wiki-js.tar.gz -C $DOCUMENTROOT
 
 # Configure Wiki.js
 cp -p $DOCUMENTROOT/config.sample.yml $DOCUMENTROOT/config.yml
+
+# Enable DB access for Wiki.js
 sed -i '' -e "29s/.*/  user: $USER/" $DOCUMENTROOT/config.yml
 sed -i '' -e "30s/.*/  pass: $PASS/" $DOCUMENTROOT/config.yml
 sed -i '' -e "31s/.*/  db: $DB/" $DOCUMENTROOT/config.yml
+
+# Enable and configure SSL
+sed -i '' -e "61s/.*/  enabled: true" $DOCUMENTROOT/config.yml
+sed -i '' -e "71s/.*/  key: $SSL_KEY" $DOCUMENTROOT/config.yml
+sed -i '' -e "61s/.*/  cert: $SSL_CERT" $DOCUMENTROOT/config.yml
+
+# Make Wiki.js bind to the current IP
+sed -i '' -e "99s/.*/bindIP: $IPV4" $DOCUMENTROOT/config.yml
 
 # Apply the correct acces rights to our documentroot
 chmod -R 750 $DOCUMENTROOT
